4386dfbe6b1df9e5ad6600e5ad4f74a393717921
Erik <45368867+eriklz@users.noreply.github.com>
2022-05-16 14:47:18 +0200

    feat: add ECS task definition

--- my-container-infrastructure/app.py
+++ my-container-infrastructure/app.py
@@ -2,7 +2,12 @@
 import os
 from aws_cdk import ( App, Environment, Stack )
 from aws_cdk.aws_ec2 import ( Vpc )
-from containers.container_management import add_cluster
+from containers.container_management import (
+    add_cluster,
+    add_task_definition_with_container,
+    ContainerConfig,
+    TaskConfig
+)
 
 
 app = App()
@@ -15,4 +20,10 @@ vpc = Vpc.from_lookup(stack, 'vpc', is_default=True)
 id = 'my-test-cluster'
 add_cluster(stack, id, vpc)
 
+task_config = TaskConfig(cpu=512, memory_limit_mb=1024, family='webserver');
+container_config = ContainerConfig(dockerhub_image='httpd')
+add_task_definition_with_container(stack,
+                                   f'taskdef-{task_config.family}',
+                                   task_config=task_config,
+                                   container_config=container_config)
 app.synth()
--- my-container-infrastructure/containers/container_management.py
+++ my-container-infrastructure/containers/container_management.py
@@ -2,8 +2,37 @@ from dataclasses import dataclass
 import string
 from constructs import Construct
 from aws_cdk.aws_ec2 import IVpc
-from aws_cdk.aws_ecs import Cluster
+from aws_cdk.aws_ecs import (
+    Cluster,
+    ContainerImage,
+    FargateTaskDefinition,
+    TaskDefinition
+)
 
 def add_cluster(scope: Construct, id: str, vpc: IVpc) -> Cluster:
     return Cluster(scope, id, vpc=vpc)
+
+@dataclass
+class TaskConfig:
+    cpu: int
+    memory_limit_mb: int
+    family: str
+
+@dataclass
+class ContainerConfig:
+    dockerhub_image: str
+
+
+def add_task_definition_with_container(scope: Construct, 
+                                       id: str,
+                                       task_config: TaskConfig,
+                                       container_config: ContainerConfig) -> TaskDefinition:
+    taskdef = FargateTaskDefinition(scope, 
+                                    id,
+                                    cpu=task_config.cpu,
+                                    memory_limit_mib=task_config.memory_limit_mb,
+                                    family=task_config.family)
+    image = ContainerImage.from_registry(container_config.dockerhub_image)
+    taskdef.add_container(f'container-{container_config.dockerhub_image}', image=image)
+    return taskdef
 
