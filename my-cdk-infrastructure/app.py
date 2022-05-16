103598f1a74c9c2bfe2138f8337cf28fe631b330
Erik <45368867+eriklz@users.noreply.github.com>
2022-05-16 14:24:46 +0200

    feat: add EC2 instance role allow login to machine

--- my-cdk-infrastructure/app.py
+++ my-cdk-infrastructure/app.py
@@ -2,17 +2,24 @@
 import os
 import aws_cdk as cdk
 import aws_cdk.aws_ec2 as ec2
+import aws_cdk.aws_iam as iam
 
 
 app = cdk.App()
 stack = cdk.Stack(app, 'my-stack', 
                   env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'),
                                       region=os.getenv('CDK_DEFAULT_REGION')))
+role = iam.Role(stack, 'ec2-role', 
+                assumed_by=iam.ServicePrincipal('ec2.amazonaws.com'),
+                managed_policies=[iam.ManagedPolicy.from_aws_managed_policy_name('AmazonSSMManagedInstanceCore')])
+
 vpc = ec2.Vpc.from_lookup(stack, 'my-vpc', is_default=True)
+
 ec2.Instance(stack, 'my-ec2', 
              instance_type=ec2.InstanceType.of(instance_class=ec2.InstanceClass.BURSTABLE2,
                                                instance_size=ec2.InstanceSize.MICRO),
              machine_image=ec2.MachineImage.latest_amazon_linux(),
+             role=role,
              vpc=vpc)
 
 app.synth()
